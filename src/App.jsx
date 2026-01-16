1b0aeb196b0ad13a24c2f359407ad1f8b9909f09
Zachary <zbergman2701@gmail.com>
2026-01-15 22:13:39 -0500

    Whacking some moles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export default function App() {
   return <></>;
 }
+import { useGame } from "./GameContext";
+
+import Field from "./Field";
+import Scoreboard from "./Scoreboard";
+import Welcome from "./Welcome";
+
+export default function App() {
+  const { playing } = useGame();
+  return (
+    <>
+      <h1>Whack a Mole</h1>
+      {playing ? (
+        <>
+          <Scoreboard />
+          <Field />
+        </>
+      ) : (
+        <Welcome />
+      )}
+    </>
+  );
+}
